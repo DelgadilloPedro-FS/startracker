cc42e8821d9f80d57638368af58df0b3f2faba7f
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-03-03 20:24:36 -0600

    fix controllers to have the same logic for the ui

--- controllers/planet.js
+++ controllers/planet.js
@@ -1,104 +1,133 @@
-// Import the Planet model
-const { Planet, Star } = require('../models')
+const { Planet, Star, StarsPlanets, Sequelize } = require("../models/index");
 
 // Show all resources
 const index = async (req, res) => {
-  // Respond with an array and 2xx status code
-  try {
-    const planets = await Planet.findAll()
-    res.status(200).render('views/Planet/index.html.twig', {
-      title: 'Planets',
-      planets: planets
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
-}
-
-// Show form to create new resource
-const showForm = async(req, res) => {
-  const { id } = req.params;
-  let planet;
-  let selectedStarIds = [];
-  if (id) {
-    planet = await Planet.findOne({ where: { id }, include: [
+  const planets = await Planet.findAll({
+    include: [
       {
         model: Star,
-        through: {attributes: []}
-      }
-    ] });
-    selectedStarIds = planet.Stars.map(star => star.id);
+        through: { attributes: [] },
+      },
+    ],
+  });
+  if (res.locals.asJson) {
+    // Respond with an array and 2xx status code
+    res.status(200).json(planets);
+    return;
+  }
+
+  res.status(200).render("views/Planet/index.html.twig", {
+    planets,
+  });
+};
+
+// Show resource
+const show = async (req, res) => {
+  const { id } = req.params;
+  const planet = await Planet.findOne({
+    where: { id },
+    include: [
+      {
+        model: Star,
+        through: { attributes: [] },
+      },
+    ],
+  });
+  if (res.locals.asJson) {
+    // Respond with a single object and 2xx code
+    res.status(200).json(planet);
+    return;
+  }
+
+  res.status(200).render("views/Planet/planet.html.twig", {
+    planet,
+  });
+};
+
+const form = async (req, res) => {
+  const { id } = req.params || -1;
+  let planet;
+  let selectedStarIds = [];
+  if (id >= 0) {
+    planet = await Planet.findOne({
+      where: { id },
+      include: [
+        {
+          model: Star,
+          through: { attributes: [] },
+        },
+      ],
+    });
+    selectedStarIds = planet.Stars.map((star) => star.id);
   }
   const stars = await Star.findAll();
-  res.render("views/Planet/planetForm.html.twig", { planet, stars, selectedStarIds });
-} 
-
-// Show one resource
-const show = async (req, res) => {
-  // Respond with a single object and 2xx code
-  const { id } = req.params
-  try {
-    const planet = await Planet.findByPk(id)
-    if (!planet) {
-      res.status(404).json({ error: 'Planet not found' })
-    } else {
-      res.status(200).json(planet)
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message })
-  }
-}
-
+  res.render("views/Planet/planetForm.html.twig", {
+    planet,
+    stars,
+    selectedStarIds,
+  });
+};
 
 // Create a new resource
-const create = async (req, res) => {
-  // Issue a redirect with a success 2xx code
-  try {
-    const newPlanet = await Planet.create(req.body)
-    res.status(201).json(newPlanet)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
+const create = async (req, res, next) => {
+  const { body } = req;
+  const { starIds } = body;
+
+  const planet = await Planet.create(body);
+  await planet.addStars(starIds);
+
+  req.planetId = planet.id;
+  next();
+
+  if (res.locals.asJson) {
+    // Issue a redirect with a success 2xx code
+    res.redirect(201, "/planets");
+    return;
   }
-}
+
+  res.redirect(303, `/planets`);
+};
 
 // Update an existing resource
-const update = async (req, res) => {
-  // Respond with a single resource and 2xx code
-  const { id } = req.params
-  try {
-    const [updated] = await Planet.update(req.body, {
-      where: { id: id }
-    })
-    if (updated) {
-      const updatedPlanet = await Planet.findByPk(id)
-      res.status(200).json(updatedPlanet)
-    } else {
-      res.status(404).json({ error: 'Planet not found' })
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message })
+const update = async (req, res, next) => {
+  const { body } = req;
+  const { id } = req.params;
+  const starIds =
+    typeof body.starIds === "string" ? [body.starIds] : body.starIds || [];
+  await Planet.update(body, { where: { id } });
+  starIds.forEach(async (starId) => {
+    await StarsPlanets.findOrCreate({ where: { planetId: id, starId } });
+  });
+  const Op = Sequelize.Op;
+  await StarsPlanets.destroy({
+    where: { planetId: id, starId: { [Op.notIn]: starIds } },
+  });
+
+  req.planetId = id;
+  next();
+
+  if (res.locals.asJson) {
+    // Respond with a single resource and 2xx code
+    res.status(200).json(`/planets/${req.params.id}`);
+    return;
   }
-}
+
+  res.redirect(303, `/planets`);
+};
 
 // Remove a single resource
 const remove = async (req, res) => {
-  // Respond with a 2xx status code and bool
-  const { id } = req.params
-  try {
-    const deleted = await Planet.destroy({
-      where: { id: id }
-    })
-    if (deleted) {
-      return res.status(201).json({ success: true });
-    } else {
-      res.status(404).json({ error: 'Planet not found' })
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message })
+  const { id } = req.params;
+  await StarsPlanets.destroy({ where: { planetId: id } });
+  await Planet.destroy({ where: { id } });
+  if (res.locals.asJson) {
+    // Respond with a 2xx status code and bool
+    res.status(204).json(true);
+    return;
   }
-}
 
-
+  res.redirect(303, "/planets");
+};
 
 // Export all controller actions
-module.exports = { index, show, create, update, remove, showForm }
+module.exports = { index, show, form, create, update, remove };
--- controllers/star.js
+++ controllers/star.js
@@ -1,82 +1,156 @@
-// Import the Star model
-const { Star } = require('../models')
-
-
+const {
+  Star,
+  Planet,
+  Galaxy,
+  StarsPlanets,
+  Sequelize,
+} = require("../models/index");
 
 // Show all resources
 const index = async (req, res) => {
-  // Respond with an array and 2xx status code
-  try {
-    const stars = await Star.findAll()
-    res.status(200).json(stars)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
+  const stars = await Star.findAll({
+    include: [
+      {
+        model: Planet,
+        through: { attributes: [] },
+      },
+      {
+        model: Galaxy,
+      },
+    ],
+  });
+  if (res.locals.asJson) {
+    // Respond with an array and 2xx status code
+    res.status(200).json(stars);
+    return;
   }
-}
+
+  res.status(200).render("views/Star/stars.html.twig", {
+    stars,
+  });
+};
 
 // Show resource
 const show = async (req, res) => {
-  // Respond with a single object and 2xx code
-  const { id } = req.params
-  try {
-    const star = await Star.findByPk(id)
-    if (!star) {
-      res.status(404).json({ error: 'Star not found' })
-    } else {
-      res.status(200).json(star)
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message })
+  const { id } = req.params;
+  const star = await Star.findOne({
+    where: { id },
+    include: [
+      {
+        model: Planet,
+        through: { attributes: [] },
+      },
+      {
+        model: Galaxy,
+      },
+    ],
+  });
+  if (res.locals.asJson) {
+    // Respond with a single object and 2xx code
+    res.status(200).json(star);
+    return;
   }
-}
+
+  res.status(200).render("views/Star/star.html.twig", {
+    star,
+  });
+};
+
+const form = async (req, res) => {
+  const { id } = req.params || -1;
+  let star;
+  let selectedPlanetIds = [];
+  if (id >= 0) {
+    star = await Star.findOne({
+      where: { id },
+      include: [
+        {
+          model: Planet,
+          through: { attributes: [] },
+        },
+      ],
+    });
+    selectedPlanetIds = star.Planets.map((star) => star.id);
+  }
+  const planets = await Planet.findAll();
+  const galaxies = await Galaxy.findAll();
+  res.render("views/Star/starForm.html.twig", {
+    star,
+    planets,
+    galaxies,
+    selectedPlanetIds,
+  });
+};
 
 // Create a new resource
-const create = async (req, res) => {
-  // Issue a redirect with a success 2xx code
-  try {
-    const newStar = await Star.create(req.body)
-    res.status(201).json(newStar)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
+const create = async (req, res, next) => {
+  const { body } = req;
+  const { planetIds } = body;
+  const tempStar = {
+    ...body,
+    galaxyId: body.galaxyId ? body.galaxyId : null,
+  };
+  const star = await Star.create(tempStar);
+  await star.addPlanets(planetIds);
+
+  req.starId = star.id;
+  next();
+
+  if (res.locals.asJson) {
+    // Issue a redirect with a success 2xx code
+    res.redirect(201, "/stars");
+    return;
   }
-}
+
+  res.redirect(303, "/stars");
+};
 
 // Update an existing resource
-const update = async (req, res) => {
-  // Respond with a single resource and 2xx code
-  const { id } = req.params
-  try {
-    const [updated] = await Star.update(req.body, {
-      where: { id: id }
-    })
-    if (updated) {
-      const updatedStar = await Star.findByPk(id)
-      res.status(200).json(updatedStar)
-    } else {
-      res.status(404).json({ error: 'Star not found' })
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message })
+const update = async (req, res, next) => {
+  const { body } = req;
+  const { id } = req.params;
+  const tempStar = {
+    ...body,
+    galaxyId: body.galaxyId ? body.galaxyId : null,
+  };
+  const planetIds =
+    typeof body.planetIds === "string"
+      ? [body.planetIds]
+      : body.planetIds || [];
+  await Star.update(tempStar, { where: { id } });
+  planetIds.forEach(async (planetId) => {
+    await StarsPlanets.findOrCreate({ where: { planetId, starId: id } });
+  });
+  const Op = Sequelize.Op;
+  await StarsPlanets.destroy({
+    where: { planetId: { [Op.notIn]: planetIds }, starId: id },
+  });
+
+  req.starId = id;
+  next();
+
+  if (res.locals.asJson) {
+    // Respond with a single resource and 2xx code
+    res.status(200).json(`/stars/${req.params.id}`);
+    return;
   }
-}
+
+  res.redirect(303, `/stars`);
+};
 
 // Remove a single resource
 const remove = async (req, res) => {
-  // Respond with a 2xx status code and bool
-  const { id } = req.params
-  try {
-    const deleted = await Star.destroy({
-      where: { id: id }
-    })
-    if (deleted) {
-      return res.status(201).json({ success: true });
-    } else {
-      res.status(404).json({ error: 'Star not found' })
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message })
+  const { id } = req.params;
+  await StarsPlanets.destroy({ where: { starId: id } });
+  await Star.destroy({ where: { id } });
+  if (res.locals.asJson) {
+    // Respond with a 2xx status code and bool
+    res.status(204).json(true);
+    return;
   }
-}
+
+  res.redirect(303, "/stars");
+};
 
 // Export all controller actions
-module.exports = { index, show, create, update, remove }
+module.exports = { index, show, form, create, update, remove };
